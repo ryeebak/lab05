a4721c03aef1f72e64f5b25cfd206e91685d03a7
ryeebak <ryeebak@gmail.com>
2023-03-24 15:01:16 +0300

    Hello world with better code style!

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-   cout << "Hello, world!" << endl;
-   return 0;
-
+	std::string name;
+	std::cout << "What is your name?\n";
+	std::cin >> name;
+	std::cout << "Hello world from @" << name << std::endl;
+	return 0;
 }
