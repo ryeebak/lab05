aa73a2c6012437169092571041593fe28e8c8c07
ryeebak <ryeebak@gmail.com>
2023-03-30 16:42:57 +0300

    added headers

---
+++ headers/formatter_ex.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+std::ostream& formatter(std::ostream& out, const std::string& message);
+
---
+++ headers/solver.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void solve(float a, float b, float c, float& x1, float& x2);
+
